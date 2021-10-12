778ca1350149915b18d90677dba283a29924bb8b
Farouk <farouk_jr@hotmail.com>
2021-10-12 13:44:41 -0500

    Added done btn functionality

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ class Todo {
         dateDiv.innerHTML = date
         todoDiv.classList.add("text-box")
         todoDiv.classList.remove("todo")
+        todoDiv.classList.remove("todo-color")
         dateDiv.classList.remove("todo")
+        dateDiv.classList.remove("todo-color")
         dateDiv.classList.add("date-box")
         box.appendChild(todoDiv)
         box.appendChild(dateDiv)
@@ -42,12 +44,35 @@ class Todo {
     doneBtn(){
         let btn = document.createElement("button")
         btn.classList.add("done-btn")
+        btn.classList.add("btn-todo-color")
+        btn.addEventListener("click", ()=>{this.done(btn)})
         return btn
     }
 
     createDiv(){
         let div = document.createElement("div")
         div.classList.add("todo")
+        div.classList.add("todo-color")
         return div
+    }
+
+    done(btn){
+        let todo = btn.parentElement
+        todo.classList.remove("todo-color")
+        todo.classList.add("done-color")
+        btn.removeEventListener("click", ()=>{this.done(btn)})
+        btn.addEventListener("click", ()=>{this.notDone(btn)})
+        btn.classList.remove("btn-todo-color")
+        btn.classList.add("btn-done-color")
+    }
+    
+    notDone(btn){
+        let todo = btn.parentElement
+        todo.classList.remove("done-color")
+        todo.classList.add("todo-color")
+        btn.removeEventListener("click", ()=>{this.notDone(btn)})
+        btn.addEventListener("click", ()=>{this.done(btn)})
+        btn.classList.remove("btn-done-color")
+        btn.classList.add("btn-todo-color")
     }
 }
